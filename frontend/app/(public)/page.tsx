50e4fd63d19bd6fe9b99a8f336aa5a24bdc6c215
naajissiddiqui <naajissiddiqui123@gmail.com>
2026-08-11 23:44:24 +0530

    chore: initialize Chronos project

---
+++ frontend/app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex-1 flex items-center justify-center p-8">
+      <h1 className="text-2xl font-bold">Chronos</h1>
+    </main>
+  );
+}
